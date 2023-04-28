80c645666788e653b0924b6040c14aac0e8dc710
GarwBow <112127867+GarwBow@users.noreply.github.com>
2023-04-28 09:38:05 -0500

    Update Main.java
    Everything seems to work fine.

--- Main.java
+++ Main.java
@@ -1,19 +1,28 @@
 import java.util.Scanner;
-class Main {
-{   
-   public static void main(String[] args) 
-   {  
+class Main
+{
+  
+   public static void main(String[] args)
+  {
       final int QUARTS_IN_GALLON = 4;
-      int quartsNeeded = 18;
+      int quartsNeeded = 0;
       int gallonsNeeded;
       int extraQuartsNeeded;
       Scanner input = new Scanner(System.in);
-      System.out.print("Enter quarts needed >> ");
-      quartsNeeded = input.nextInt();
-      gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
-      extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
-      System.out.println("A job that needs " + quartsNeeded +
-         " quarts requires " + gallonsNeeded + " gallons plus " +
-         extraQuartsNeeded + " quarts.");
+         try
+         {
+            System.out.print("Enter quarts needed >> ");
+            quartsNeeded = input.nextInt();
+            gallonsNeeded = quartsNeeded / QUARTS_IN_GALLON;
+            extraQuartsNeeded = quartsNeeded % QUARTS_IN_GALLON;
+            System.out.println("A job that needs " + quartsNeeded +
+            " quarts requires " + gallonsNeeded + " gallons plus " +
+            extraQuartsNeeded + " quarts.");
+         } 
+         catch (Exception e) 
+         {
+            System.out.println("Not a valid input, please input a number");
+         }
+        System.out.println("End of program");
    }
 }
